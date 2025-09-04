7198a5623dc07055ecc47c126030b4a73bfb1058
vix_hentx <w1084349470@outlook.com>
2025-09-04 22:50:36 +0800

    Add UI Project

---
+++ RPMailUI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace RPMailUI.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ RPMailUI/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace RPMailUI.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
