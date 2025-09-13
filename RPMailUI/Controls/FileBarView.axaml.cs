fbdc90a70725698f08984d214482f0744a65bf7e
vix_hentx <w1084349470@outlook.com>
2025-09-13 19:20:16 +0800

    FileBarView complete

--- RPMailUI/Controls/FileBarView.axaml.cs
+++ RPMailUI/Controls/FileBarView.axaml.cs
@@ -1,6 +1,12 @@
+using System;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Controls.Primitives;
+using Avalonia.Platform.Storage;
+using CommunityToolkit.Mvvm.Input;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
+using RPMailUI.Utils;
 
 namespace RPMailUI.Controls;
 
@@ -24,8 +30,89 @@ public partial class FileBarView : UserControl
         set => SetValue(CaptionProperty, value);
     }
 
+    public static readonly StyledProperty<string> FileTypeProperty = AvaloniaProperty.Register<FileBarView, string>(
+        nameof(FileType),"");
+
+    public string FileType
+    {
+        get => GetValue(FileTypeProperty);
+        set => SetValue(FileTypeProperty, value);
+    }
+
+    public static readonly StyledProperty<bool> IsDirectoryProperty = AvaloniaProperty.Register<FileBarView, bool>(
+        nameof(IsDirectory));
+
+    public bool IsDirectory
+    {
+        get => GetValue(IsDirectoryProperty);
+        set => SetValue(IsDirectoryProperty, value);
+    }
+
     public FileBarView()
     {
         InitializeComponent();
+    }
+
+    [RelayCommand]
+    private async Task Browse()
+    {
+        var topLevel = TopLevel.GetTopLevel(this)!;
+        var provider = topLevel.StorageProvider;
+
+        if (!IsDirectory)
+        {
+            var fileTypeExt = string.IsNullOrWhiteSpace(FileType) ? "*" : FileType.ToLower();
+            var fileTypeName = $"{(string.IsNullOrWhiteSpace(FileType) ? "Any" : FileType)} File";
+            var files = await provider.OpenFilePickerAsync(new ()
+            {
+                Title = $"Open {FileType} File",
+                AllowMultiple = false,
+                FileTypeFilter = [new(fileTypeName)
+                {
+                    Patterns = [$"*.{fileTypeExt}"]
+                }]
+            });
+        
+            if (files.Count > 0)
+            {
+                FilePath = files[0].TryGetLocalPath() ?? "";
+            }
+        }
+        else
+        {
+            var directories = await provider.OpenFolderPickerAsync(new()
+            {
+                Title = "Select Directory",
+                AllowMultiple = false,
+                SuggestedStartLocation = await provider.TryGetFolderFromPathAsync(AppContext.BaseDirectory)
+            });
+
+            if (directories.Count > 0)
+            {
+                FilePath = directories[0].TryGetLocalPath() ?? "";
+            }
+        }
+    }
+
+    [RelayCommand]
+    private async Task Open()
+    {
+        try
+        {
+            if (!IsDirectory)
+            {
+                PathOpenHelper.OpenFilePath(FilePath);
+            }
+            else
+            {
+                PathOpenHelper.OpenDirectory(FilePath);
+            }
+        }
+        catch (Exception ex)
+        {
+            var box = MessageBoxManager
+                .GetMessageBoxStandard("Error", $"Cannot open {FilePath}: {ex.Message}");
+            await box.ShowAsync();
+        }
     }
 }
---
+++ RPMailUI/Utils/PathOpenHelper.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace RPMailUI.Utils;
+
+public static class PathOpenHelper
+{
+    private static readonly Action<string> _openFileMethod;
+    private static readonly Action<string> _openDirectoryMethod;
+    public static void OpenFilePath(string path) => _openFileMethod(path);
+    public static void OpenDirectory(string path) => _openDirectoryMethod(path);
+
+    static PathOpenHelper()
+    {
+        if (OperatingSystem.IsWindows())
+        {
+            _openFileMethod = path => Process.Start(new ProcessStartInfo
+            {
+                FileName = "explorer.exe",
+                Arguments = $"/select, \"{path}\"",
+                UseShellExecute = true
+            });
+            _openDirectoryMethod = path => Process.Start(new ProcessStartInfo
+            {
+                FileName = "explorer.exe",
+                Arguments = path,
+                UseShellExecute = true
+            });
+        }
+        else if (OperatingSystem.IsLinux())
+        {
+            _openFileMethod = path => 
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = "xdg-open",
+                    Arguments = $"\"{path}\"",
+                    UseShellExecute = true
+                });
+            };
+
+            _openDirectoryMethod = path => Process.Start(new ProcessStartInfo
+            {
+                FileName = "xdg-open",
+                Arguments = $"\"{path}\"",
+                UseShellExecute = true
+            });
+        }
+        else if (OperatingSystem.IsMacOS())
+        {
+            _openFileMethod = path => Process.Start(new ProcessStartInfo
+            {
+                FileName = "open",
+                Arguments = $"-R \"{path}\"",
+                UseShellExecute = true
+            });
+
+            // macOS: 打开目录
+            _openDirectoryMethod = path => Process.Start(new ProcessStartInfo
+            {
+                FileName = "open",
+                Arguments = $"\"{path}\"",
+                UseShellExecute = true
+            });
+        }
+        else
+        {
+            throw new PlatformNotSupportedException();
+        }
+    }
+}
