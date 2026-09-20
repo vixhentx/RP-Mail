60e84eb7182b21156e29eb4a5bad53fd15c25913
vix_hentx <w1084349470@outlook.com>
2026-09-21 00:48:21 +0800

    chore: bump version to 27.0.1

--- nix/packages/ui/package.nix
+++ nix/packages/ui/package.nix
@@ -19,7 +19,7 @@
 }:
 buildDotnetModule {
   pname = "rpmail-ui";
-  version = "27.0.0";
+  version = "27.0.1";
   inherit src;
   projectFile = "RPMailUI/RPMailUI.csproj";
   dotnet-sdk = dotnetCorePackages.sdk_10_0;
